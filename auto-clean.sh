54cffeb13f066296e2d74e4131784d129cdc2e6a
batokenms <69089713+batokenms@users.noreply.github.com>
2023-05-02 22:50:20 -0500

    Update auto-clean.sh
    Check if you are the Root

--- auto-clean.sh
+++ auto-clean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Check if you are the Root
+if ["${EUID}" -ne 0 ]; then
+    echo "Please run as the Root"
+    exit 
+
+else
+    echo "You are the Root and you are good to go"
+fi
+
 # Update packages and install security patches
 sudo apt-get update
 sudo apt-get upgrade -y
